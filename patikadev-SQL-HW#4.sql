5b39b55a00a0b194e9ff10add5bf16b8028a405c
Erkan Aksoy <51321673+rknksy@users.noreply.github.com>
2023-12-19 17:13:30 +0300

    Create patikadev-SQL-HW#4.sql

---
+++ patikadev-SQL-HW#4.sql
@@ -0,0 +1,19 @@
+--patika.dev SQL HW#3
+--1.
+SELECT DISTINCT replacement_cost FROM film;
+--2
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+--3
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+--4
+SELECT COUNT(country) FROM country
+WHERE country LIKE '_____';
+--5
+SELECT COUNT(city) FROM city
+WHERE city ILIKE '%r';
+
+
+
+
+
